4a27867e7f01ef59990dfcfe35959af312a8adfc
Pig Fang <g-plane@hotmail.com>
2021-08-30 00:25:51 +0800

    add basic UI layout

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider, Center, HStack } from '@chakra-ui/react'
 
 const MonacoEditor = React.lazy(() => import('@monaco-editor/react'))
 
 const App: React.FC = () => (
   <ChakraProvider>
-    <div>hi</div>
-    <MonacoEditor height="400px" width="400px" defaultLanguage="javascript" />
+    <Center>
+      <HStack width="88vw" height="88vh" spacing="60px">
+        <Box width="100%" height="100%" borderWidth="thin" borderColor="gray.400">
+          <React.Suspense fallback={''}>
+            <MonacoEditor width="100%" height="100%" />
+          </React.Suspense>
+        </Box>
+        <Box width="100%" height="100%" borderWidth="thin" borderColor="gray.400">
+          <React.Suspense fallback={''}>
+            <MonacoEditor width="100%" height="100%" defaultLanguage="typescript" />
+          </React.Suspense>
+        </Box>
+      </HStack>
+    </Center>
   </ChakraProvider>
 )
 

9c842e6e5db8111e357325b301138bc315b0006c
Pig Fang <g-plane@hotmail.com>
2021-08-30 22:57:34 +0800

    serialize code to URL

--- playground/src/components/TypacroEditor.tsx
+++ playground/src/components/TypacroEditor.tsx
@@ -1,12 +1,36 @@
 import * as React from 'react'
 import { useAtom } from 'jotai'
 import { Box } from '@chakra-ui/react'
+import { gzip, ungzip } from 'pako'
+import { Base64 } from 'js-base64'
 import { sourceCodeAtom } from '../states/codeAtom'
 
 const MonacoEditor = React.lazy(() => import('@monaco-editor/react'))
 
 const TypacroEditor: React.FC = () => {
   const [sourceCode, setSourceCode] = useAtom(sourceCodeAtom)
+
+  React.useEffect(() => {
+    const url = new URL(location.href)
+    const encodedCode = url.searchParams.get('code')
+    if (!encodedCode) {
+      return
+    }
+
+    try {
+      const decoded = ungzip(Base64.toUint8Array(encodedCode), { to: 'string' })
+      setSourceCode(decoded)
+    } catch {
+      // ignore
+    }
+  }, [])
+
+  React.useEffect(() => {
+    const gzippedCode = Base64.fromUint8Array(gzip(sourceCode), true)
+    const url = new URL(location.href)
+    url.searchParams.set('code', gzippedCode)
+    history.pushState(null, '', url.toString())
+  }, [sourceCode])
 
   const handleEditorValueChange = (value?: string) => {
     if (value != null) {

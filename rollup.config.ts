bf5016ae7dd0e14fd079ec56b4719ac8404c73bd
Pig Fang <g-plane@hotmail.com>
2021-09-01 17:04:14 +0800

    use Rollup to build library

---
+++ rollup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import swc from 'rollup-plugin-swc'
+
+export default defineConfig({
+  input: './src/index.ts',
+  output: {
+    file: './dist/typacro.mjs',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    resolve({
+      extensions: ['.ts'],
+    }),
+    swc({
+      jsc: {
+        parser: {
+          syntax: 'typescript',
+        },
+        loose: true,
+        target: 'es2018',
+      },
+      sourceMaps: true,
+    }),
+  ],
+  external: ['acorn'],
+})
